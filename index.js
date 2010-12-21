a4fa355d7f1a02b508dbd287fcc9086352f6b41f
Daniel Lo <wilburlo@gmail.co>
2010-12-20 23:48:16 -0800

    inital commit of code

---
+++ index.js
@@ -0,0 +1,24 @@
+// system libraries
+var fs = require('fs');
+
+// our own libraries
+var APP_MODULES = require(__dirname + '/lib');
+
+// load our configuration
+try {
+  var configJSON = fs.readFileSync(__dirname + '/config.json');
+  config = JSON.parse(configJSON.toString());
+} catch(e) {
+  console.log("File /config.json not loadable");
+}
+
+// make sure we have a method
+if ( process.argv[2] in APP_MODULES ) {
+    console.log('Running module:' + process.argv[2])
+    console.log('arguments:' + process.argv.slice(3))
+    var APP = APP_MODULES[process.argv[2]]
+    APP.config = config
+    APP.run(process.argv.slice(3))
+} else {
+    console.log(process.argv[2] + ' is not available in APP_MODULES')
+}
